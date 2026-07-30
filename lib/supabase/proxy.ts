91d9909d11c4038c3f27ca7ba967403d8d3e47b0
Nati-darse <natnaeldarsema@gmail.com>
2026-07-30 15:40:15 +0300

    add the route for api testing

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -55,6 +55,7 @@ export async function updateSession(request: NextRequest) {
      !request.nextUrl.pathname.startsWith("/products")&&
      !request.nextUrl.pathname.startsWith("/dashboard")&&
      !request.nextUrl.pathname.startsWith("/featured")&&
+      !request.nextUrl.pathname.startsWith("/api") && 
    !request.nextUrl.pathname.startsWith("/auth")
   ) {
     // no user, potentially respond by redirecting the user to the login page
